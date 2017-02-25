c01b65254a81d09304585269833428e748dee274
roringbbk <bbk.programmerz@gmail.com>
2017-02-25 17:12:47 +0545

    Add redcarpet and pygment for markdown and syntax highlighting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,29 @@
 module ApplicationHelper
-  def user_signed_in?
-    if current_user
-      return true
-    else
-      return false
+  # def user_signed_in?
+  #   if current_user
+  #     return true
+  #   else
+  #     return false
+  #   end
+  # end
+
+  class HTMLwithPygments < Redcarpet::Render::HTML
+    def block_code(code, language)
+      Pygments.highlight(code, lexer: language)
     end
+  end
+
+  def markdown(content)
+    renderer = HTMLwithPygments.new(hard_wrap: true, filter_html: true)
+    options = {
+      autolink: true,
+      no_intra_emphasis: true,
+      disable_indented_code_blocks: true,
+      fenced_code_blocks: true,
+      lax_html_blocks: true,
+      strikethrough: true,
+      superscript: true  
+    }
+    Redcarpet::Markdown.new(renderer, options).render(content).html_safe
   end
 end
